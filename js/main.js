af219206453712f77762cb5273c4ff0c93185d09
Vytautas <vyckelis@gmail.com>
2022-07-15 16:58:40 +0300

    chekIfNotEmpty

--- js/main.js
+++ js/main.js
@@ -13,12 +13,52 @@ function formSubmitHandler(event) {
   // console.log('js works');
   // .trim() - nuimam tuscius tarpus is abieju pusiu
   const loginValue = loginEl.value.trim();
+  const passwordValue = passwordEl.value.trim();
+
   console.log('loginValue ===', loginValue);
   loginEl.value = loginValue;
-  if (loginValue.length < 3) {
-    console.warn('reiktu daugiau raidikiu');
-    return;
-  }
+  // email
+  if (checkIfNotEmpty(loginEl)) return;
+
+  if (checkIfNotLessThen(loginEl)) return;
+  // password
+  if (checkIfNotEmpty(passwordEl)) return;
+
+  if (checkIfNotLessThen(passwordEl)) return;
+
+  // if (passwordValue.length === 0) {
+  //   console.warn('ivesk kanors');
+  //   passwordEl.nextElementSibling.style.display = 'block';
+  //   passwordEl.nextElementSibling.textContent = 'ivesk kanors';
+  //   passwordEl.className = 'input-error';
+  //   return;
+  // }
 
   console.log('siunciam forma!!!!!!!!');
+}
+loginEl.addEventListener('blur', () => {
+  if (checkIfNotEmpty(loginEl)) return;
+
+  if (checkIfNotLessThen(loginEl)) return;
+});
+
+function checkIfNotEmpty(inputEl) {
+  const inputElValue = inputEl.value.trim();
+  if (inputElValue.length === 0) {
+    console.warn('ivesk kanors');
+    inputEl.nextElementSibling.style.display = 'block';
+    inputEl.nextElementSibling.textContent = 'ivesk kanors';
+    inputEl.className = 'input-error';
+    return true;
+  }
+}
+function checkIfNotLessThen(inputEl) {
+  const inputElValue = inputEl.value.trim();
+  if (inputElValue.length < 3) {
+    console.warn('reiktu daugiau raidikiu');
+    inputEl.nextElementSibling.style.display = 'block';
+    inputEl.nextElementSibling.textContent = 'reiktu daugiau raidikiu';
+    inputEl.className = 'input-error';
+    return true;
+  }
 }
